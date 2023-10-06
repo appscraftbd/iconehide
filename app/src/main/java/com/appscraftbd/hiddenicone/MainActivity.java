16c3e24152428b46dfd820f23e2fcbef07c54a27
appscraft.bd <appscraftbd@gmail.com>
2023-10-07 01:10:10 +0600

    v 1.0

---
+++ app/src/main/java/com/appscraftbd/hiddenicone/MainActivity.java
@@ -0,0 +1,52 @@
+package com.appscraftbd.hiddenicone;
+
+import static android.Manifest.permission.READ_CALL_LOG;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+}
